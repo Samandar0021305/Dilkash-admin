77778674989d7e44ff456301f8330c575da7c8a4
Samandar0021305 <TSamandardev89@gmail.com>
2023-01-23 14:34:52 +0500

    commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <>
+    hello world
+    </>
+  );
+}
+
+export default App;
---
+++ src/_nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const _nav = [
+  {
+    component: CNavItem,
+    name: 'Dashboard',
+    to: '/dashboard',
+  },
+]
+
+export default _nav
