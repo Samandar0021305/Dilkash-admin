4ad19bbd512d2f15e8ef95899ab4f297b1de7142
umidkhan <umidxondeveloper@gmail.com>
2023-01-24 23:19:51 +0500

    fix sidebar

---
+++ src/utils/Constants.js
@@ -0,0 +1,23 @@
+// import dashboardIcon from "https://icons8.com/icon/1737/speedometer";
+// import dropIcon from "https://icons8.com/icon/2980/blur";
+// import pencilIcon from "https://icons8.com/icon/mYDrPkARiuEU/edit";
+// import pieceIcon from "https://icons8.com/icon/1775/puzzle";
+// import cursorIcon from ".https://uxwing.com/wp-content/themes/uxwing/download/arrow-direction/direction-corner-top-right-icon.png";
+
+export const sidebar = [
+  "Home Dashboard",
+  "NFT Marketplace",
+  "Data tables",
+  "Profile",
+  "Sign In",
+  "RTL Admin",
+];
+
+// export const sidebarIcons = [
+//   dashboardIcon,
+//   dropIcon,
+//   pencilIcon,
+//   pieceIcon,
+//   cursorIcon,
+//   // homeLockIcon,
+// ];
