271cc47bf8ee29d84ed53d1ebf1853c25405e9e5
Dilrux2007 <urazaliyevdilrux@gmail.com>
2023-01-25 21:52:53 +0500

    old code delete

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,23 +6,6 @@ import {buttons, words, icons} from "../navbarContents/navbarContents"
 export default function Navbar() {
     return (
         <>
-            {/* <nav style={{ display: "flex", justifyContent: "space-between" }}>
-            <div style={{display: "flex", justifyContent: "space-around", alignItems: "center"}}>
-                <Button style={{ border: "none", color: "#000000", background: "#FFFFFF" }}><MenuIcon /></Button>
-                <Button style={{ border: "none", color: "#000000", background: "#FFFFFF" }}>Dashboard</Button>
-                <Button style={{ border: "none", color: "#000000", background: "#FFFFFF" }}>Users</Button>
-                <Button style={{ border: "none", color: "#000000", background: "#FFFFFF" }}>Setting</Button>
-            </div>
-
-            <div>
-                <div className="search" style={{ display: "flex", alignItems: "center", padding: "10px" }}>
-                    <Button style={{ border: "none", color: "#000000", background: "#FFFFFF" }}><NotificationsNoneIcon /></Button>
-                    <Button style={{ border: "none", color: "#000000", background: "#FFFFFF" }}><FormatListBulletedIcon /></Button>
-                    <Button style={{ border: "none", color: "#000000", background: "#FFFFFF" }}><DraftsOutlinedIcon /></Button>
-                    <div className="ava" style={{ border: 'none', borderRadius: "100%", background: "purple", padding: "10px", color: "#FFFFFF" }}>GG</div>
-                </div>
-            </div>
-            </nav> */}
             <nav className='flex justify-between'>
             <div style={{display: "flex", justifyContent: "space-around", alignItems: "center"}}>
                 {buttons.map((items) => {
