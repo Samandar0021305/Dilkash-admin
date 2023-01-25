a5ff49b9aef805c259eb60e27555ce3bfee8b5b2
Asliddin2002 <asliddinwiutstudent@gmail.com>
2023-01-25 12:23:41 +0500

    added: category pagination

--- src/modules/category.api.js
+++ src/modules/category.api.js
@@ -3,18 +3,31 @@ import configureApi from "../api";
 const categoryEndpoints = {
   list: "/category",
   getOne: (categId) => `/category/${categId}`,
-  add: "category/create",
+  add: "/category/create",
   updated: ({ categId }) => `/category/update/${categId}`,
   delete: ({ categId }) => `/category/delete/${categId}`,
+  pagination: ({ page, pageSize }) =>
+    `/category?page=${page}&pageSize=${pageSize}`,
 };
 
 const categoryApi = {
-  getCategory: async () => {
-    try {
-      const response = await configureApi.get(categoryEndpoints.list);
-      return response;
-    } catch (error) {
-      return error;
+  getCategory: async ({ page, pageSize }) => {
+    if (page && pageSize) {
+      try {
+        const response = await configureApi.get(
+          categoryEndpoints.pagination(page, pageSize)
+        );
+        return response;
+      } catch (error) {
+        return error;
+      }
+    } else {
+      try {
+        const response = await configureApi.get(categoryEndpoints.list);
+        return response;
+      } catch (error) {
+        return error;
+      }
     }
   },
   updateCateg: async ({ categId }) => {
