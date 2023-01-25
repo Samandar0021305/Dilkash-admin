a2acc2d8472fb0a47617b45e3e74532d91f1a283
Asliddin2002 <asliddinwiutstudent@gmail.com>
2023-01-25 22:25:31 +0500

    Revert "fix"
    This reverts commit eb3009e9ed54553e1b94cb008478b69dea98153c.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,25 +1,18 @@
-import React from "react";
+import React from 'react'
 
 export default function Footer() {
   return (
     <>
-      <div className="flex justify-between">
-        <div className="flex">
-          <p className="text-blue-600 cursor-pointer underline">CoreUI </p>
-          <p className="text-sm text-gray-700 font-medium flex items-center ">
-            {" "}
-            © 2022 creativeLabs
-          </p>
-        </div>
-        <div className="flex">
-          <p className="text-sm text-gray-700 font-medium flex items-center">
-            Powered by
-          </p>
-          <p className="text-blue-600 cursor-pointer underline">
-            CoreUI ReactAdmin & Dashboard Template
-          </p>
-        </div>
-      </div>
+    <div className='flex justify-between'>
+    <div className='flex'>
+      <p className='text-blue-600 cursor-pointer underline'>CoreUI </p>
+      <p className='text-sm text-gray-700 font-medium flex items-center '> © 2022 creativeLabs</p>
+    </div>
+    <div className='flex'>
+      <p className='text-sm text-gray-700 font-medium flex items-center'>Powered by</p> 
+      <p className='text-blue-600 cursor-pointer underline'>CoreUI ReactAdmin & Dashboard Template</p>
+    </div>
+    </div>
     </>
-  );
+  )
 }
