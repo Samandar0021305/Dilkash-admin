8901207ef95e76305dd059251d310ad998a2faec
umidkhan <umidxondeveloper@gmail.com>
2023-01-27 12:06:41 +0500

    sidebar fixed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 
 import Navbar from "./components/Navbar"
+import Sidebar from "./components/Sidebar/Sidebar";
 const Routers = React.lazy(()=>import("./rootes/Index"))
 
 function App() {
   return (
     <>
-    <Navbar />
+      <Navbar />
+      <Sidebar />
     <Routers />
     </>
   );
---
+++ src/utils/Constants.js
@@ -0,0 +1,38 @@
+export const sidebar = [
+  {
+    name: "Home Dashboard",
+    icon: "fa-solid fa-house",
+  },
+  {
+    name: "Colors",
+    icon: "fa-solid fa-droplet",
+  },
+  {
+    name: "Typography",
+    icon: "fa-solid fa-pencil",
+  },
+  {
+    name: "Base",
+    icon: "fa-solid fa-puzzle-piece",
+  },
+  {
+    name: "Buttons",
+    icon: "fa-solid fa-user",
+  },
+  {
+    name: "Forms",
+    icon: "fa-solid fa-file-lines",
+  },
+  {
+    name: "Charts",
+    icon: "fa-solid fa-chart-pie",
+  },
+  {
+    name: "Pages",
+    icon: "fa-solid fa-star ",
+  },
+  {
+    name: "Docs",
+    icon: "fa-solid fa-book",
+  },
+];
