1e4024c55e58810f322ef8b2e5eca007da73f6c6
Samandar0021305 <TSamandardev89@gmail.com>
2023-02-09 16:15:41 +0500

    commit

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import React, { useReducer } from 'react'
-import Sidebar from "../components/Sidebar/Sidebar"
-=======
-import React from 'react'
-import Saidbar from "../components/Sidebar/Sidebar"
-import Navbar from "../components/navbar/Navbar"
->>>>>>> 0eaada37dcc6804e72afc326465d180b0186bae1
-
+const Sidebar = React.lazy(()=>import("../components/Sidebar/Sidebar"))
 const Navbar = React.lazy(()=>import("../components/navbar/Navbar"))
 const Footer = React.lazy(()=>import("../components/footer/Footer"))
 const IsPage =  React.lazy(()=>import("../components/IsPage"))
@@ -27,8 +20,9 @@ const reducerFunction = (state,action)=>{
 const Index = ({children}) => {
    const [isToogle,setToogle] = useReducer(reducerFunction,initialValue)
   return (
-<<<<<<< HEAD
+ 
    <Context.Provider value={{setToogle:setToogle,isToogle:isToogle}}>
+
      <div className='flex'>
           <Sidebar /> 
          <div className='ml-[10px] w-[100%] pr-[10px]'>
@@ -36,17 +30,9 @@ const Index = ({children}) => {
             <IsPage/>
                <main className='min-h-[86.5vh]'>{children}</main>
             <Footer />
-=======
-    <div className='flex'>
-            <Saidbar/>
-         <div className='ml-[10px]'>
-            <nav><Navbar/></nav>
-               <main>{children}</main>
-            <footer></footer>
->>>>>>> 0eaada37dcc6804e72afc326465d180b0186bae1
          </div>
     </div>
-   </Context.Provider>
+   </Context.Provider> 
   )
 }
 
