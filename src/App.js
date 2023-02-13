b1806466790bf9d7ce137de422b45ab1b94708fe
Samandar0021305 <TSamandardev89@gmail.com>
2023-02-13 18:44:18 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
+// import Login from "./page/Login/Login"
 const RouteEL = React.lazy(()=>import("./rootes"))
 
 function App() {
   return (
     <>
     <RouteEL />
+    {/* <Login /> */}
     </>
   );
 }
